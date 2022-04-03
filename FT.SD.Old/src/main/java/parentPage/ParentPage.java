0f86ad85d00c5fac5337c7446efbd5d0eeadb7ec
AndrewSushyi <sushyi_av@ukr.net>
2022-04-03 16:04:31 +0300

    add Action methods in Test + modify subPOM + LogProperty

--- FT.SD.Old/src/main/java/parentPage/ParentPage.java
+++ FT.SD.Old/src/main/java/parentPage/ParentPage.java
@@ -1,14 +1,20 @@
 package parentPage;
 
 
-import org.apache.log4j.Logger;
+
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ParentPage {
     //объект на браузер: запустить браузер, открыть в нем страницу,найти элемен, закрыть браузер
     protected WebDriver webDriver;
-    protected Logger logger = Logger.getLogger(getClass());
+//    protected Logger logger = Logger.getLogger(getClass().getName());
+protected Logger logger = LogManager.getLogger(getClass());
+
 
 
     //конструктор: сразу передаем драйвер браузера
--- FT.SD.Old/src/test/java/loginTest/LoginTest.java
+++ FT.SD.Old/src/test/java/loginTest/LoginTest.java
@@ -3,8 +3,6 @@ package loginTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -13,40 +11,40 @@ import java.io.File;
 import java.time.Duration;
 
 public class LoginTest {
-    WebDriver webDriver;
-
-    @Test
-    public void validLogin() {
-
-        File file = new File(".\\src\\drivers\\chromedriver.exe");
-        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-
-        webDriver = new ChromeDriver();
-        webDriver.manage().window().maximize();
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5)); // явное ожидание
-        webDriver.get("http://service-desk.test.k8s.mc.gcf/");
-        webDriver.findElement(By.xpath("//dx-button[@text='Login']")).click();
-        //sign in page
-        webDriver.findElement(By.id("username")).clear();
-        webDriver.findElement(By.id("username")).sendKeys("");
-        webDriver.findElement(By.id("password")).clear();
-        webDriver.findElement(By.id("password")).sendKeys("");
-        webDriver.findElement(By.name("login")).click();
-        //проверка
-        Assert.assertTrue(isAvatarPresent(), "avatar is not present");
-
-        webDriver.quit();
-
-    }
-
-    private boolean isAvatarPresent() {
-        try {
-            return webDriver.findElement(By.className("toolbar-avatar")).isDisplayed();
-
-        }catch (Exception e){
-            return false;
-
-        }
-    }
+//    WebDriver webDriver;
+//
+//    @Test
+//    public void validLogin() {
+//
+//        File file = new File(".\\src\\drivers\\chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+//
+//        webDriver = new ChromeDriver();
+//        webDriver.manage().window().maximize();
+//        webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5)); // явное ожидание
+//        webDriver.get("http://service-desk.test.k8s.mc.gcf/");
+//        webDriver.findElement(By.xpath("//dx-button[@text='Login']")).click();
+//        //sign in page
+//        webDriver.findElement(By.id("username")).clear();
+//        webDriver.findElement(By.id("username")).sendKeys("");
+//        webDriver.findElement(By.id("password")).clear();
+//        webDriver.findElement(By.id("password")).sendKeys("");
+//        webDriver.findElement(By.name("login")).click();
+//        //проверка
+//        Assert.assertTrue(isAvatarPresent(), "avatar is not present");
+//
+//        webDriver.quit();
+//
+//    }
+//
+//    private boolean isAvatarPresent() {
+//        try {
+//            return webDriver.findElement(By.className("toolbar-avatar")).isDisplayed();
+//
+//        }catch (Exception e){
+//            return false;
+//
+//        }
+//    }
 }
